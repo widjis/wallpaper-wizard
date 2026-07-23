a0b2b672c60e55cda327250f8209c3097e8fdf96
widjis <widji.santoso@gmail.com>
2026-07-23 21:46:23 +0800

    modif vite

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
     // nitro/vite builds from this
     server: { entry: "server" },
   },
+  nitro: {
+    preset: "node-server",
+  },
   vite: {
     preview: {
       allowedHosts: ["mtiwallpaper.merdekabattery.com", "localhost", "127.0.0.1"],
